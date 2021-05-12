e8c61adea6e8e56de383cc31c15977c57028e604
triszt4n <trisztanpiller@gmail.com>
2021-05-13 00:22:19 +0200

    :lemon: Example modified

--- src/main/kotlin/com/example/demo/MyApp.kt
+++ src/main/kotlin/com/example/demo/MyApp.kt
@@ -1,11 +1,6 @@
 package com.example.demo
 
 import com.example.demo.main.MainView
-import javafx.application.Application
 import tornadofx.App
 
 class MyApp : App(MainView::class, Styles::class)
-
-fun main(args: Array<String>) {
-    Application.launch(MyApp::class.java, *args)
-}
